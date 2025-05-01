a6cdfc4e0bc1d2ade4a5a57df5f8cab34a229fd2
Manoj Kumar <manoj.jmp@gmail.com>
2025-05-01 11:52:53 +0530

    initial version

---
+++ AsyncDemoSolution/Demo.Data.Contracts/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.Data.Contracts.Models;
+
+namespace Demo.Data.Contracts
+{
+    public interface IProductRepository
+    {
+        List<Product> GetProducts();
+        void AddProduct(Product product);
+    }
+}
